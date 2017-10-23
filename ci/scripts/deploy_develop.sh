4f1cf59f26da8568a1fd98e6349d304776aaafc3
Hector Rondon <hrondon@teclib.com>
2017-10-23 13:57:27 -0400

    docs(license): update header for the copyright information

--- ci/scripts/deploy_develop.sh
+++ ci/scripts/deploy_develop.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-#   Copyright © 2017 Teclib. All rights reserved.
+# LICENSE
 #
-# deploy_develop.sh is part of Glpi
-#
-# Glpi is a subproject of Flyve MDM. Flyve MDM is a mobile
-# device management software.
+# deploy_develop.sh is part of the GLPI API Client Library for Swift,
+# a subproject of GLPI. GLPI is a free IT Asset Management.
 #
 # Glpi is Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,12 +17,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ------------------------------------------------------------------------------
-# @author    Hector Rondon
+# @author    Hector Rondon - <hrondon@teclib.com>
 # @date      08/09/17
-# @copyright Copyright © 2017 Teclib. All rights reserved.
+# @copyright (C) 2017 Teclib' and contributors
 # @license   Apache License, Version 2.0 https://www.apache.org/licenses/LICENSE-2.0
 # @link      https://github.com/flyve-mdm/ios-library-glpi
-# @link      https://flyve-mdm.com
+# @link      http://www.glpi-project.org/
 # ------------------------------------------------------------------------------
 
 GITHUB_COMMIT_MESSAGE=$(git log --format=oneline -n 1 $CIRCLE_SHA1)

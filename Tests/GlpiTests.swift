7490baf1a95cd957c8d7f8aa3729d3058fe7f181
Hector Rondon <hrondon@teclib.com>
2017-11-02 12:22:21 -0400

    test(api): run unit without external dependencies

--- Tests/GlpiTests.swift
+++ Tests/GlpiTests.swift
@@ -27,7 +27,6 @@
 
 import XCTest
 @testable import Glpi
-import Alamofire
 
 class GlpiTests: XCTestCase {
     
@@ -43,33 +42,34 @@ class GlpiTests: XCTestCase {
     
     /// Test initSession request
     func testInitSession() {
-        
+
         let expectationResult = expectation(description: "initSession")
-        
+
         let expectedUserToken = Constants.initSessionTesting["userToken"] ?? ""
         let expectedAppToken = Constants.initSessionTesting["appToken"] ?? ""
-        let expectedMethod = Constants.initSessionTesting["method"] ?? ""
         
-        Alamofire.request(Routers.initSessionByUserToken(expectedUserToken, expectedAppToken).request()).response { response in
-
-            XCTAssertEqual(response.request?.value(forHTTPHeaderField: "Content-Type") ?? "", "application/json")
-            XCTAssertEqual(response.request?.value(forHTTPHeaderField: "Authorization") ?? "", "user_token \(expectedUserToken)")
-            XCTAssertEqual(response.request?.httpMethod ?? "", expectedMethod)
-            XCTAssertEqual(response.response?.statusCode ?? 0, 200)
-            expectationResult.fulfill()
-        }
+        GlpiRequest.initSessionByUserToken(userToken: expectedUserToken, appToken: expectedAppToken, completion: { _, response, _ in
+            
+            if let urlResponse: HTTPURLResponse = response {
+                XCTAssertEqual(urlResponse.allHeaderFields["Content-Type"] as? String ?? "", "application/json; charset=UTF-8")
+                XCTAssertEqual(urlResponse.statusCode, 200)
+                expectationResult.fulfill()
+            }
+        })
         waitForExpectations(timeout: 10, handler: nil)
     }
-    
+
     /// Test KillSession request
     func testKillSession() {
-        
+
         let expectationResult = expectation(description: "killSession")
-        
-        Alamofire.request(Routers.killSession.request()).response { response in
-            XCTAssertEqual(response.request?.value(forHTTPHeaderField: "Content-Type") ?? "", "application/json")
-            XCTAssertEqual(response.request?.httpMethod ?? "", "GET")
-            expectationResult.fulfill()
+
+        GlpiRequest.killSession { _, response, _ in
+            
+            if let urlResponse: HTTPURLResponse = response {
+                XCTAssertEqual(urlResponse.allHeaderFields["Content-Type"] as? String ?? "", "application/json; charset=UTF-8")
+                expectationResult.fulfill()
+            }
         }
         waitForExpectations(timeout: 10, handler: nil)
     }
@@ -79,262 +79,246 @@ class GlpiTests: XCTestCase {
         
         let expectationResult = expectation(description: "getMyProfiles")
         
-        Alamofire.request(Routers.getMyProfiles.request()).response { response in
-            XCTAssertEqual(response.request?.value(forHTTPHeaderField: "Content-Type") ?? "", "application/json")
-            XCTAssertEqual(response.request?.httpMethod ?? "", "GET")
-            expectationResult.fulfill()
+        GlpiRequest.getMyProfiles { _, response, _ in
+            
+            if let urlResponse: HTTPURLResponse = response {
+                XCTAssertEqual(urlResponse.allHeaderFields["Content-Type"] as? String ?? "", "application/json; charset=UTF-8")
+                expectationResult.fulfill()
+            }
         }
-        waitForExpectations(timeout: 10, handler: nil)
+        waitForExpectations(timeout: 1000, handler: nil)
     }
     
     /// Test getActiveProfile request
     func testGetActiveProfile() {
-        
+
         let expectationResult = expectation(description: "getActiveProfile")
-        
-        Alamofire.request(Routers.getActiveProfile.request()).response { response in
-            XCTAssertEqual(response.request?.value(forHTTPHeaderField: "Content-Type") ?? "", "application/json")
-            XCTAssertEqual(response.request?.httpMethod ?? "", "GET")
-            expectationResult.fulfill()
+
+        GlpiRequest.getActiveProfile { _, response, _ in
+            
+            if let urlResponse: HTTPURLResponse = response {
+                XCTAssertEqual(urlResponse.allHeaderFields["Content-Type"] as? String ?? "", "application/json; charset=UTF-8")
+                expectationResult.fulfill()
+            }
         }
         waitForExpectations(timeout: 10, handler: nil)
     }
-    
+
     /// Test changeActiveProfile request
     func testChangeActiveProfile() {
-        
+
         let expectationResult = expectation(description: "changeActiveProfile")
         
-        var dictionary = [String: AnyObject]()
-        dictionary["profiles_id"] = 4 as AnyObject
-        
-        Alamofire.request(Routers.changeActiveProfile(dictionary).request()).response { response in
-            XCTAssertEqual(response.request?.value(forHTTPHeaderField: "Content-Type") ?? "", "application/json")
-            XCTAssertEqual(response.request?.httpMethod ?? "", "POST")
+        GlpiRequest.changeActiveProfile(profileID: "4") { _, response, _ in
             
-            let jsonData = try? JSONSerialization.data(withJSONObject: dictionary, options: [])
-            let jsonString = String(data: jsonData!, encoding: .utf8)!
-
-            XCTAssertEqual(String(data: (response.request?.httpBody)!, encoding: .utf8) ?? "", jsonString)
-
-            expectationResult.fulfill()
+            if let urlResponse: HTTPURLResponse = response {
+                XCTAssertEqual(urlResponse.allHeaderFields["Content-Type"] as? String ?? "", "application/json; charset=UTF-8")
+                expectationResult.fulfill()
+            }
         }
         waitForExpectations(timeout: 10, handler: nil)
     }
-    
+
     /// Test getMyEntities request
     func testGetMyEntities() {
-        
+
         let expectationResult = expectation(description: "getMyEntities")
         
-        Alamofire.request(Routers.getMyEntities.request()).response { response in
-            XCTAssertEqual(response.request?.value(forHTTPHeaderField: "Content-Type") ?? "", "application/json")
-            XCTAssertEqual(response.request?.httpMethod ?? "", "GET")
-            expectationResult.fulfill()
+        GlpiRequest.getMyEntities { _, response, _ in
+            
+            if let urlResponse: HTTPURLResponse = response {
+                XCTAssertEqual(urlResponse.allHeaderFields["Content-Type"] as? String ?? "", "application/json; charset=UTF-8")
+                expectationResult.fulfill()
+            }
         }
         waitForExpectations(timeout: 10, handler: nil)
     }
-    
+
     /// Test getActiveEntities request
     func testGetActiveEntities() {
-        
+
         let expectationResult = expectation(description: "getActiveEntities")
         
-        Alamofire.request(Routers.getActiveEntities.request()).response { response in
-            XCTAssertEqual(response.request?.value(forHTTPHeaderField: "Content-Type") ?? "", "application/json")
-            XCTAssertEqual(response.request?.httpMethod ?? "", "GET")
-            expectationResult.fulfill()
+        GlpiRequest.getActiveEntities { _, response, _ in
+            
+            if let urlResponse: HTTPURLResponse = response {
+                XCTAssertEqual(urlResponse.allHeaderFields["Content-Type"] as? String ?? "", "application/json; charset=UTF-8")
+                expectationResult.fulfill()
+            }
         }
         waitForExpectations(timeout: 10, handler: nil)
     }
-    
+
     /// Test changeActiveEntities request
     func testChangeActiveEntities() {
-        
+
         let expectationResult = expectation(description: "changeActiveEntities")
         
-        var dictionary = [String: AnyObject]()
-        dictionary["entities_id"] = 1 as AnyObject
-        
-        Alamofire.request(Routers.changeActiveEntities(dictionary).request()).response { response in
-            XCTAssertEqual(response.request?.value(forHTTPHeaderField: "Content-Type") ?? "", "application/json")
-            XCTAssertEqual(response.request?.httpMethod ?? "", "POST")
+        GlpiRequest.changeActiveEntities(entitiesID: "0", isRecursive: false, completion: { _, response, _ in
             
-            let jsonData = try? JSONSerialization.data(withJSONObject: dictionary, options: [])
-            let jsonString = String(data: jsonData!, encoding: .utf8)!
-
-            XCTAssertEqual(String(data: (response.request?.httpBody)!, encoding: .utf8) ?? "", jsonString)
-            
-            expectationResult.fulfill()
-        }
+            if let urlResponse: HTTPURLResponse = response {
+                XCTAssertEqual(urlResponse.allHeaderFields["Content-Type"] as? String ?? "", "application/json; charset=UTF-8")
+                expectationResult.fulfill()
+            }
+        })
         waitForExpectations(timeout: 10, handler: nil)
     }
-    
+
     /// Test getFullSession request
     func testGetFullSession() {
-        
+
         let expectationResult = expectation(description: "getFullSession")
         
-        Alamofire.request(Routers.getFullSession.request()).response { response in
-            XCTAssertEqual(response.request?.value(forHTTPHeaderField: "Content-Type") ?? "", "application/json")
-            XCTAssertEqual(response.request?.httpMethod ?? "", "GET")
-            expectationResult.fulfill()
+        GlpiRequest.getFullSession { _, response, _ in
+            
+            if let urlResponse: HTTPURLResponse = response {
+                XCTAssertEqual(urlResponse.allHeaderFields["Content-Type"] as? String ?? "", "application/json; charset=UTF-8")
+                expectationResult.fulfill()
+            }
         }
         waitForExpectations(timeout: 10, handler: nil)
     }
-    
+
     /// Test getAllItems request
     func testGetAllItems() {
-        
+
         let expectationResult = expectation(description: "getAllItems")
         
-        Alamofire.request(Routers.getAllItems(ItemType.Computer, nil).request()).response { response in
-            XCTAssertEqual(response.request?.value(forHTTPHeaderField: "Content-Type") ?? "", "application/json")
-            XCTAssertEqual(response.request?.httpMethod ?? "", "GET")
-            expectationResult.fulfill()
-        }
+        GlpiRequest.getAllItems(itemType: ItemType.Computer, queryString: nil, completion: { _, response, _ in
+            
+            if let urlResponse: HTTPURLResponse = response {
+                XCTAssertEqual(urlResponse.allHeaderFields["Content-Type"] as? String ?? "", "application/json; charset=UTF-8")
+                expectationResult.fulfill()
+            }
+        })
         waitForExpectations(timeout: 10, handler: nil)
     }
-    
+
     /// Test getAnItems request
     func testGetAnItems() {
-        
+
         let expectationResult = expectation(description: "getAnItems")
-        
-        Alamofire.request(Routers.getItem(ItemType.Computer, 119, nil).request()).response { response in
-            XCTAssertEqual(response.request?.value(forHTTPHeaderField: "Content-Type") ?? "", "application/json")
-            XCTAssertEqual(response.request?.httpMethod ?? "", "GET")
-            expectationResult.fulfill()
-        }
+
+        GlpiRequest.getItem(itemType: ItemType.Computer, itemID: 119, queryString: nil, completion: { _, response, _ in
+            
+            if let urlResponse: HTTPURLResponse = response {
+                XCTAssertEqual(urlResponse.allHeaderFields["Content-Type"] as? String ?? "", "application/json; charset=UTF-8")
+                expectationResult.fulfill()
+            }
+        })
         waitForExpectations(timeout: 10, handler: nil)
     }
-    
+
     /// Test getsubItems
     func testGetSubItems() {
-        
+
         let expectationResult = expectation(description: "getsubItems")
         
-        Alamofire.request(Routers.getSubItems(ItemType.User, 24, ItemType.UserEmail, nil).request()).response { response in
-            XCTAssertEqual(response.request?.value(forHTTPHeaderField: "Content-Type") ?? "", "application/json")
-            XCTAssertEqual(response.request?.httpMethod ?? "", "GET")
-            expectationResult.fulfill()
-        }
+        GlpiRequest.getSubItems(itemType: ItemType.User, itemID: 24, subItemType: ItemType.UserEmail, queryString: nil, completion: { _, response, _ in
+            
+            if let urlResponse: HTTPURLResponse = response {
+                XCTAssertEqual(urlResponse.allHeaderFields["Content-Type"] as? String ?? "", "application/json; charset=UTF-8")
+                expectationResult.fulfill()
+            }
+        })
         waitForExpectations(timeout: 10, handler: nil)
     }
-    
+
     /// Test addItems
     func testAddItems() {
-        
+
         let expectationResult = expectation(description: "addItems")
-        
+
         var dictionary = [String: AnyObject]()
         var dictionaryData = [String: String]()
         dictionaryData["name"] = "Hector Rondon"
         dictionary["input"] = dictionaryData as AnyObject
         
-        Alamofire.request(Routers.addItems(ItemType.User, dictionary).request()).response { response in
-            XCTAssertEqual(response.request?.value(forHTTPHeaderField: "Content-Type") ?? "", "application/json")
-            XCTAssertEqual(response.request?.httpMethod ?? "", "POST")
+        GlpiRequest.addItems(itemType: ItemType.User, payload: dictionary, completion: { _, response, _ in
             
-            let jsonData = try? JSONSerialization.data(withJSONObject: dictionary, options: [])
-            let jsonString = String(data: jsonData!, encoding: .utf8)!
-            
-            XCTAssertEqual(String(data: (response.request?.httpBody)!, encoding: .utf8) ?? "", jsonString)
-            
-            expectationResult.fulfill()
-        }
+            if let urlResponse: HTTPURLResponse = response {
+                XCTAssertEqual(urlResponse.allHeaderFields["Content-Type"] as? String ?? "", "application/json; charset=UTF-8")
+                expectationResult.fulfill()
+            }
+        })
         waitForExpectations(timeout: 10, handler: nil)
     }
-    
+
     /// Test updateItems
     func testUpdateItems() {
-        
+
         let expectationResult = expectation(description: "updateItems")
-        
+
         var dictionary = [String: AnyObject]()
         var dictionaryData = [String: String]()
         dictionaryData["name"] = "Hector Rondon Update"
         dictionary["input"] = dictionaryData as AnyObject
         
-        Alamofire.request(Routers.updateItems(ItemType.User, 24, dictionary).request()).response { response in
-            XCTAssertEqual(response.request?.value(forHTTPHeaderField: "Content-Type") ?? "", "application/json")
-            XCTAssertEqual(response.request?.httpMethod ?? "", "PUT")
+        GlpiRequest.updateItems(itemType: ItemType.User, itemID: 24, payload: dictionary, completion: { _, response, _ in
             
-            let jsonData = try? JSONSerialization.data(withJSONObject: dictionary, options: [])
-            let jsonString = String(data: jsonData!, encoding: .utf8)!
-            
-            XCTAssertEqual(String(data: (response.request?.httpBody)!, encoding: .utf8) ?? "", jsonString)
-            
-            expectationResult.fulfill()
-        }
+            if let urlResponse: HTTPURLResponse = response {
+                XCTAssertEqual(urlResponse.allHeaderFields["Content-Type"] as? String ?? "", "application/json; charset=UTF-8")
+                expectationResult.fulfill()
+            }
+        })
+
         waitForExpectations(timeout: 10, handler: nil)
     }
-    
+
     /// Test deleteItems
     func testDeleteItems() {
-        
+
         let expectationResult = expectation(description: "deleteItems")
-        
+
         var dictionary = [String: AnyObject]()
         var dictionaryData = [String: Int]()
         dictionaryData["id"] = 24
         dictionary["input"] = dictionaryData as AnyObject
         
-        Alamofire.request(Routers.deleteItems(ItemType.User, nil, nil, dictionary).request()).response { response in
-            XCTAssertEqual(response.request?.value(forHTTPHeaderField: "Content-Type") ?? "", "application/json")
-            XCTAssertEqual(response.request?.httpMethod ?? "", "DELETE")
+        GlpiRequest.deleteItems(itemType: ItemType.User, itemID: 24, queryString: nil, payload: dictionary, completion: { _, response, _ in
             
-            let jsonData = try? JSONSerialization.data(withJSONObject: dictionary, options: [])
-            let jsonString = String(data: jsonData!, encoding: .utf8)!
-            
-            XCTAssertEqual(String(data: (response.request?.httpBody)!, encoding: .utf8) ?? "", jsonString)
-            
-            expectationResult.fulfill()
-        }
+            if let urlResponse: HTTPURLResponse = response {
+                XCTAssertEqual(urlResponse.allHeaderFields["Content-Type"] as? String ?? "", "application/json; charset=UTF-8")
+                expectationResult.fulfill()
+            }
+        })
         waitForExpectations(timeout: 10, handler: nil)
     }
-    
+
     /// Test lostPassword
     func testLostPassword() {
         let expectationResult = expectation(description: "lostPassword")
-        
+
         var dictionary = [String: AnyObject]()
         dictionary["email"] = "flyve@hi2.in" as AnyObject
         
-        Alamofire.request(Routers.lostPassword(dictionary).request()).response { response in
-            XCTAssertEqual(response.request?.value(forHTTPHeaderField: "Content-Type") ?? "", "application/json")
-            XCTAssertEqual(response.request?.httpMethod ?? "", "PUT")
+        GlpiRequest.recoveryPassword(email: "flyve@hi2.in", completion: { _, response, _ in
             
-            let jsonData = try? JSONSerialization.data(withJSONObject: dictionary, options: [])
-            let jsonString = String(data: jsonData!, encoding: .utf8)!
-            
-            XCTAssertEqual(String(data: (response.request?.httpBody)!, encoding: .utf8) ?? "", jsonString)
-            
-            expectationResult.fulfill()
-        }
+            if let urlResponse: HTTPURLResponse = response {
+                XCTAssertEqual(urlResponse.allHeaderFields["Content-Type"] as? String ?? "", "application/json; charset=UTF-8")
+                expectationResult.fulfill()
+            }
+        })
         waitForExpectations(timeout: 10, handler: nil)
     }
-    
+
     /// Test resetPassword
     func testResetPassword() {
         let expectationResult = expectation(description: "resetPassword")
-        
+
         var dictionary = [String: AnyObject]()
         dictionary["email"] = "flyve@hi2.in" as AnyObject
         dictionary["password_forget_token"] = "941891fcf47581084d43d2dec49b1c43a58380f4" as AnyObject
         dictionary["password"] = "12345678" as AnyObject
         
-        Alamofire.request(Routers.lostPassword(dictionary).request()).response { response in
-            XCTAssertEqual(response.request?.value(forHTTPHeaderField: "Content-Type") ?? "", "application/json")
-            XCTAssertEqual(response.request?.httpMethod ?? "", "PUT")
+        GlpiRequest.resetPassword(payload: dictionary, completion: { _, response, _ in
             
-            let jsonData = try? JSONSerialization.data(withJSONObject: dictionary, options: [])
-            let jsonString = String(data: jsonData!, encoding: .utf8)!
-            
-            XCTAssertEqual(String(data: (response.request?.httpBody)!, encoding: .utf8) ?? "", jsonString)
-            
-            expectationResult.fulfill()
-        }
+            if let urlResponse: HTTPURLResponse = response {
+                XCTAssertEqual(urlResponse.allHeaderFields["Content-Type"] as? String ?? "", "application/json; charset=UTF-8")
+                expectationResult.fulfill()
+            }
+        })
         waitForExpectations(timeout: 10, handler: nil)
     }
 }
+

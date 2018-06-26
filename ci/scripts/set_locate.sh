03adda791a0f155ffcc325b22942128d3b1c872b
Hector Rondon <hrondon@teclib.com>
2018-06-26 15:24:31 -0400

    ci(test): migrate test to circleCI 2.0
    Signed-off-by: Hector Rondon <hrondon@teclib.com>

---
+++ ci/scripts/set_locate.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+#   LICENSE
+#
+# fastlane_config.sh is part of FlyveMDMInventoryAgent
+#
+# FlyveMDMInventoryAgent is a subproject of Flyve MDM. Flyve MDM is a mobile
+# device management software.
+#
+# FlyveMDMInventoryAgent is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# FlyveMDMInventoryAgent is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# ------------------------------------------------------------------------------
+# @author    Hector Rondon <hrondon@teclib.com>
+# @date      06/06/18
+# @copyright Copyright © 2017-2018 Teclib. All rights reserved.
+# @license   LGPLv3 https://www.gnu.org/licenses/lgpl-3.0.html
+# @link      https://github.com/flyve-mdm/ios-inventory-agent.git
+# @link      http://flyve.org/ios-inventory-agent
+# @link      https://flyve-mdm.com
+# ------------------------------------------------------------------------------
+
+echo ---------------------- UTF-8 locale --------------------------
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8

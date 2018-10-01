bc8b7cc3a8435197b086104a2fbeede511875354
Hector Rondon <hrondon@teclib.com>
2018-10-01 10:52:06 -0400

    docs(license): add license on header
    Signed-off-by: Hector Rondon <hrondon@teclib.com>

--- Source/Glpi.h
+++ Source/Glpi.h
@@ -1,29 +1,28 @@
-/*
- * LICENSE
+/**
+ *  LICENSE
  *
- * Glpi.h is part of the GLPI API Client Library for Swift,
- * a subproject of GLPI. GLPI is a free IT Asset Management.
+ *  This file is part of the GLPI API Client Library for Swift,
+ *  a subproject of GLPI. GLPI is a free IT Asset Management.
  *
- * Glpi is Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  Glpi is Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *  http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * ------------------------------------------------------------------------------
- * @author    Hector Rondon <hrondon@teclib.com>
- * @date      13/10/17
- * @copyright Copyright © 2017-2018 Teclib. All rights reserved.
- * @license   Apache License, Version 2.0 https://www.apache.org/licenses/LICENSE-2.0
- * @link      https://github.com/glpi-project/swift-library-glpi
- * @link      https://glpi-project.github.io/swift-library-glpi/
- * @link      http://www.glpi-project.org/
- * ------------------------------------------------------------------------------
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  -----------------------------------------------------------------------------------
+ *  @author    Hector Rondon - <hrondon@teclib.com>
+ *  @copyright Copyright Teclib. All rights reserved.
+ *  @license   Apache License, Version 2.0 https://www.apache.org/licenses/LICENSE-2.0
+ *  @link      https://github.com/glpi-project/swift-library-glpi
+ *  @link      https://glpi-project.github.io/swift-library-glpi/
+ *  @link      http://www.glpi-project.org/
+ *  -----------------------------------------------------------------------------------
  */
 
 #import <UIKit/UIKit.h>

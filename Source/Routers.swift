7d7746861129b52e764d07d93ffd843ab361e495
Hector Rondon <hrondon@teclib.com>
2018-10-08 17:28:01 -0400

    refactor(api): use generic query string
    Signed-off-by: Hector Rondon <hrondon@teclib.com>

--- Source/Routers.swift
+++ Source/Routers.swift
@@ -40,37 +40,37 @@ public enum Routers: URLRequestDelegate {
     /// GET /getActiveProfile
     case getActiveProfile
     /// POST /changeActiveProfile
-    case changeActiveProfile([String: AnyObject])
+    case changeActiveProfile([String: Any]?)
     /// GET /getMyEntities
-    case getMyEntities([String: AnyObject])
+    case getMyEntities([String: Any]?)
     /// GET /getActiveEntities
     case getActiveEntities
     /// POST /changeActiveEntities
-    case changeActiveEntities([String: AnyObject])
+    case changeActiveEntities([String: Any]?)
     /// GET /getFullSession
     case getFullSession
     /// GET /getGlpiConfig
     case getGlpiConfig
     /// GET /:itemtype
-    case getAllItems(ItemType, [String: String]?)
+    case getAllItems(ItemType, [String: Any]?)
     /// GET /:itemtype/:id
-    case getItem(ItemType, Int, [String: String]?)
+    case getItem(ItemType, Int, [String: Any]?)
     /// GET /:itemtype/:id/:sub_itemtype
-    case getSubItems(ItemType, Int, ItemType, [String: String]?)
+    case getSubItems(ItemType, Int, ItemType, [String: Any]?)
     /// GET /getMultipleItems
-    case getMultipleItems
+    case getMultipleItems([String: Any]?)
     /// POST /:itemtype
-    case addItems(ItemType, [String: AnyObject])
+    case addItems(ItemType, [String: Any]?)
     /// PUT /:itemtype/:id
-    case updateItems(ItemType, Int?, [String: AnyObject])
+    case updateItems(ItemType, Int?, [String: Any]?)
     /// DELETE /:itemtype/:id
-    case deleteItems(ItemType, Int?, [String: String]?, [String: AnyObject])
+    case deleteItems(ItemType, Int?, [String: Any]?, [String: Any]?)
     /// PUT /lostPassword
-    case lostPassword([String: AnyObject])
+    case lostPassword([String: Any]?)
     /// GET /listSearchOptions/:itemtype
-    case listSearchOptions(ItemType)
+    case listSearchOptions(ItemType, [String: Any]?)
     /// GET /search/:itemtype
-    case searchItems(ItemType)
+    case searchItems(ItemType, [String: Any]?)
     
     /// get HTTP Method
     var method: HTTPMethod {
@@ -136,9 +136,9 @@ public enum Routers: URLRequestDelegate {
             }
         case .lostPassword:
             return "/lostPassword"
-        case .listSearchOptions(let itemType):
+        case .listSearchOptions(let itemType, _):
             return "/listSearchOptions/\(itemType)"
-        case .searchItems(let itemType):
+        case .searchItems(let itemType, _):
             return "/search/\(itemType)"
         }
     }
@@ -148,28 +148,12 @@ public enum Routers: URLRequestDelegate {
 
         switch self {
         case .initSessionByUserToken, .initSessionByCredentials, .killSession, .getMyProfiles, .getActiveProfile,
-             .changeActiveProfile, .getMyEntities, .getActiveEntities, .changeActiveEntities,
-             .getFullSession, .getGlpiConfig, .getMultipleItems, .addItems, .updateItems, .lostPassword, .listSearchOptions, .searchItems:
+             .changeActiveProfile, .getActiveEntities, .changeActiveEntities,
+             .getFullSession, .getGlpiConfig, .addItems, .updateItems, .lostPassword:
             return  nil
-        case .getAllItems(_, let params):
-            if let queryString = params {
-                return queryString.map { URLQueryItem(name: $0.key, value: "\($0.value)") }
-            } else {
-                return nil
-            }
-        case .getItem(_, _, let params):
-            if let queryString = params {
-                return queryString.map { URLQueryItem(name: $0.key, value: "\($0.value)") }
-            } else {
-                return nil
-            }
-        case .getSubItems(_, _, _, let params):
-            if let queryString = params {
-                return queryString.map { URLQueryItem(name: $0.key, value: "\($0.value)") }
-            } else {
-                return nil
-            }
-        case .deleteItems(_, _, let params, _):
+        case .getMyEntities(let params), .getAllItems(_, let params), .getItem(_, _, let params),
+             .getSubItems(_, _, _, let params), .getMultipleItems(let params), .deleteItems(_, _, let params, _),
+             .listSearchOptions(_, let params), .searchItems(_, let params):
             if let queryString = params {
                 return queryString.map { URLQueryItem(name: $0.key, value: "\($0.value)") }
             } else {
@@ -233,11 +217,10 @@ public enum Routers: URLRequestDelegate {
         }
         
         switch self {
-        case .changeActiveProfile(let payload), .getMyEntities(let payload), .changeActiveEntities(let payload), .addItems(_, let payload), .updateItems(_, _, let payload), .lostPassword(let payload), .deleteItems(_, _, _, let payload):
+        case .changeActiveProfile(let payload), .changeActiveEntities(let payload), .addItems(_, let payload), .updateItems(_, _, let payload), .lostPassword(let payload), .deleteItems(_, _, _, let payload):
 
-            if let jsonData = try? JSONSerialization.data(withJSONObject: payload, options: []) {
+            if let jsonData = try? JSONSerialization.data(withJSONObject: payload ?? [String : Any](), options: []) {
                 if let jsonString = String(data: jsonData, encoding: .utf8) {
-                    print(jsonString)
                     urlRequest.httpBody = jsonString.data(using: .utf8)
                 }
             }
